257314ca3815545931805b0c8ce95465aef5b7b9
Rishi-Kakkar-1 <105473753+Rishi-Kakkar-1@users.noreply.github.com>
2022-05-23 23:58:32 -0400

    Added input save to text box

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import "./styles.css";
+import React from "react";
+// import ReactDOM from "react-dom";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Job Search</h1>
-      <h3>Search the company you want jobs for</h3>
-      <form>
-        <label>
-          Company:&nbsp;
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    let company = this.company.value;
+    console.log(company);
+    this.inputed.textContent = company;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Job Search</h1>
+        <h3>Search the company you want jobs for</h3>
+        <form>
+          <label>
+            Company:&nbsp;
+            <input type="text" ref={(c) => (this.company = c)} name="company" />
+          </label>
+          <input type="submit" onClick={this.onSubmit} value="Submit" />
+        </form>
+        <p ref={(c) => (this.inputed = c)} name="inputed" />
+      </div>
+    );
+  }
 }
